14168ccd4dc956a5d7314332c6e1581f4fc98740
Dmitriy Zibrov <zibrovdu@gmail.com>
2021-08-08 12:41:24 +0300

    changes: -add h2 banner; -add 2 tabs

--- car_dash/layouts.py
+++ car_dash/layouts.py
@@ -4,10 +4,33 @@ import dash_core_components as dcc
 
 def serve_layout():
     layout = html.Div([
-        dcc.Dropdown(id='1',
-                     options=[
-                         {'name': 'qwe', 'value': 1},
-                         {'name': 'asd', 'value': 2}
-                     ])
+        html.Div([
+            html.H2('HONDA The power of dreams'),
+            # html.Img(src="assets/logo.png")
+            html.A([
+                html.Img(src="assets/pixlr-bg-result.png")
+            ], href='#modal-1', className='js-modal-open link')
+        ], className="banner"),
+        html.Div([
+            html.Div([
+                dcc.Tabs(
+                    id='',
+                    value='',
+                    children=[
+                        dcc.Tab(
+                            id='fuel_tab',
+                            label='Топливо',
+                            value='fuel',
+                            children=[]
+                        ),
+                        dcc.Tab(
+                            id='repair_tab',
+                            value='repair',
+                            label='Запчасти и работы',
+                            children=[]
+                        )
+                    ])
+            ])
+        ])
     ])
     return layout
